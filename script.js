08bc8c364fd9a7e9e56d4b99fa964a801dbbf0f3
Walt Lungan <Walt Lungan>
2022-04-25 12:05:33 -0700

    added fs functions to createFood for snake game

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+
+//declaring global variables to track the game board size. maybe can use this with other gameboards?
 const linePixelCount = 40
 const totalPixelCount = linePixelCount**2
-
+//declaring variables that track score to the user
 let totalFoodEaten = 0
 let totalDistanceTraveled = 0
 
+//shortening reference to gameboard itself
 const gameContainer = document.getElementById('gameContainer')
 
-
+// function used for generating the gameboard
 const createGameBoardPixels = () => {
     for (let i = 1; i <= totalPixelCount; i++){ //this loop creates a new element with the class and id of 'i'
         gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameBoardPixel" id="${i}"></div>`
     }
 }
+
+
+//shortening reference to gamepixels
+const gameBoardPixels = document.getElementsByClassName('gameBoardPixel')
+
+
+//declaring food variable, creating function that creates new food pixel
+let currentFoodPosition = 0
+const createFood = () => {
+    gameBoardPixels[currentFoodPosition].classList.remove('food')
+}
+
+currentFoodPosition = Math.floor( Math.random() * totalPixelCount )
